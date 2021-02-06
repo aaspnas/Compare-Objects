372fcea2b9244faffeca661c0146f027d212fe15
Anders Aspnäs <aaspnas@gmail.com>
2021-02-06 22:54:46 +0200

    Implementd null values and type diffs

--- Compare-ObjectsExt.Tests.ps1
+++ Compare-ObjectsExt.Tests.ps1
@@ -1,9 +1,23 @@
 BeforeAll {
-    . $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+  #  . $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+  . .\Compare-ObjectsExt.ps1
 }
 
 Describe "Compare-ObjectsExt" {
-    It "Returns expected output" {
-        Compare-ObjectsExt | Should -Be "YOUR_EXPECTED_VALUE"
+    It "Returns expected output for null values" {
+        Compare-ObjectsExt $null $null | Should -BeNullOrEmpty
+        Compare-ObjectsExt "" "" | Should -BeNullOrEmpty
+        Compare-ObjectsExt $null "foo" | Should -Match '/ - Ref is null, but Diff has a value not null'
+        Compare-ObjectsExt "foo" $null | Should -Match '/ - Ref is not null, but Diff has a null value'
+
     }
+    It "Returns expected output for integer values" {
+        Compare-ObjectsExt 1 1 | Should -BeNullOrEmpty
+
+    }
+    It "Notices difference between different data types" {
+        Compare-ObjectsExt "foo" 1 | Should -Match '/ - Ref and Diff datatype names differ'
+
+    }
+    
 }
--- Compare-ObjectsExt.ps1
+++ Compare-ObjectsExt.ps1
@@ -1,3 +1,81 @@
 function Compare-ObjectsExt {
-    throw [NotImplementedException]'Compare-ObjectsExt is not implemented.'
+    Param(
+    [Parameter(Position=0, 
+    Mandatory=$true)]
+    [AllowEmptyString()]
+    [Alias('Reference')]
+    [AllowNull()]
+    $ref,
+
+    [Parameter(Position=1, 
+    Mandatory=$true)]
+    [AllowEmptyString()]
+    [Alias('Difference')]
+    [AllowNull()]
+    $diff,
+
+    [Parameter(Mandatory=$false)]
+    [String[]] 
+    $path ='/',
+
+    [Parameter(Mandatory=$false)]
+    [Switch]
+    $NoDetails
+)
+
+    if ($null -eq $ref -or '' -eq $ref) { 
+        if ($null -eq $diff -or '' -eq $diff) {
+            return 
+        } else {
+            return "$path - Ref is null, but Diff has a value not null"
+        }
+    } else {
+        if ($null -eq $diff -or '' -eq $diff) {
+            return "$path - Ref is not null, but Diff has a null value"
+
+        } else {
+        
+        }
+    }
+    Write-Debug "ref and diff both not null"
+    $refTypeData = ($ref) | Get-TypeData
+    $diffTypeData = ($diff) | Get-TypeData
+
+    if ($null -eq $refTypeData) {
+        $refTypeData = $ref.GetType()
+    }
+
+    if ($null -eq $diffTypeData) {
+        $diffTypeData = $diff.GetType()
+    }
+    Write-Debug "Ref type:"
+    Write-Debug "$refTypeData"
+    Write-Debug "Diff type:"
+    write-Debug "$diffTypeData"
+
+    if ($null -eq $refTypeData) {
+        if ($null -eq $refTypeData) {
+            # Datatype match $null, impossible situation
+        } else {
+            # Datatype match $null, impossible situation
+            return "$path - Ref: Ref data type is null, but Diff data type has a value not null"
+        }
+    } else {
+        if ($null -eq $refTypeData) {
+            # Datatype match $null, impossible situation
+            return "$path - Ref: Diff data type is null, but Ref data type has a value not null"
+
+        } else {
+           
+            if ($refTypeData.Name -eq $diffTypeData.Name) {
+                # looks promising, now we can examine objects
+
+            } else {
+                return "$path - Ref and Diff datatype names differ"
+            }
+
+        }
+    }
+
+    
 }
